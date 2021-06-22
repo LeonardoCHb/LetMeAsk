1c06317f275ea2e7e3454b78d591ee66d4fabe3e
LeonardoCHb <leonardohably@hotmail.com>
2021-06-22 07:06:18 -0300

    Initial folders

---
+++ src/services/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+
+import 'firebase/auth'
+import 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVSUT-sNrSWNmHgYkIBRz5jYxRdbwI4tQ",
+  authDomain: "letmeask-e45da.firebaseapp.com",
+  databaseURL: "https://letmeask-e45da-default-rtdb.firebaseio.com",
+  projectId: "letmeask-e45da",
+  storageBucket: "letmeask-e45da.appspot.com",
+  messagingSenderId: "959474801480",
+  appId: "1:959474801480:web:cb32f4c05c8aca4cfcb17d"
+};
+
+
+firebase.initializeApp(firebaseConfig)
+
+export const auth= firebase.auth()
+export const database = firebase.database()
