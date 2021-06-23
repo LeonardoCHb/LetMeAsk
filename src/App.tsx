006bf5f49e848620d488f548db91ca1fcce78236
LeonardoCHb <leonardohably@hotmail.com>
2021-06-23 10:58:23 -0300

    Aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <h1>SUP </h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
